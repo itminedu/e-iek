f5443b771b6e7b814df03cf86a370769d795f452
giannisdag <giannisdag@yahoo.gr>
2017-03-29 10:03:33 +0300

    remove app.settings.ts

--- source/app.settings.ts
+++
@@ -1,6 +0,0 @@
-export const API_ENDPOINT = 'http://localhost:8083/dist/iekserver';
-export class AppSettings {
-   public static get API_ENDPOINT(): string {
-      return 'http://localhost:8083/dist/iekserver';
-   }
-}
